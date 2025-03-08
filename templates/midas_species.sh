30a922b69d31af4577aff6a048d5509877ce159b
Sam Minot <sminot@gmail.com>
2025-03-08 00:15:58 -0800

    Update syntax for v3

--- templates/midas_species.sh
+++ templates/midas_species.sh
@@ -10,13 +10,14 @@ gunzip -c "${db}" | tar -xvf - -C DB/
 # Run the MIDAS command
 # Output to the current working directory
 echo "\$(date) Running MIDAS"
+mkdir "${sample}"
 midas run_species \
-    ./ \
+    "${sample}" \
+    --sample_name "${sample}" \
     -1 "${fastq_1}" \
     -2 "${fastq_2}" \
-    -t ${task.cpus} \
-    -d DB \
-    --remove_temp \
+    --num_cores ${task.cpus} \
+    --midasdb_dir DB \
     --word_size "${params.word_size}" \
     --aln_cov "${params.aln_cov}"
 
